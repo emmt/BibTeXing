b7c0482f46663421f7ae6f9eb265ee19baa37c0e
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2025-09-22 09:32:34 +0200

    Test fields shortcut

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,8 @@ using Test
     str = open(inp) do io; read(io, String); end
     A = tryparse(BibTeX, str)
     @test A isa BibTeX
+    @test A == A
+    @test isequal(A, A)
     @test A.preamble isa AbstractVector
     @test A.strings isa AbstractDict{Symbol}
     @test A.entries isa AbstractDict{String}
@@ -16,8 +18,28 @@ using Test
     @test A.strings[:AApL] == [:AAp, "{ Letters}"]
     @test A.strings[:Foy] == ["{Foy, Renaud}"]
     @test A.strings[:Labeyrie] == ["\"Labeyrie, Antoine\""]
-    @test A == A
-    @test isequal(A, A)
+    key = "Foy_Labeyrie-1985-LGS"
+    @test A.entries[key].type === :article
+    @test A.entries[key].key == key
+    @test A.entries[key][:author] === A.entries[key].fields[:author]
+    @test A.entries[key][:author] == [:Foy, :and, :Labeyrie]
+    @test A.entries[key][:journal] === A.entries[key].fields[:journal]
+    @test A.entries[key][:journal] == [:AAp]
+    @test A.entries[key][:year] === A.entries[key].fields[:year]
+    @test A.entries[key][:year] == [1985]
+    key = "Labeyrie-1975-Vega"
+    entry = A.entries[key]
+    @test entry.type === :article
+    @test entry.key == key
+    @test entry[:author] == [:Labeyrie]
+    @test entry[:title] == ["{Interference fringes obtained on {V}ega with two optical telescopes}"]
+    @test entry[:journal] == [:ApJL]
+    @test entry[:year] == [1975]
+    @test entry[:volume] == [196]
+    @test entry[:number] == [2]
+    @test entry[:pages] == ["{L71--L75}"]
+    @test entry[:doi] == ["{10.1086/181747}"]
+    @test entry[:adsurl] == ["{http://cdsads.u-strasbg.fr/abs/1975ApJ...196L..71L}"]
 
     B = @inferred BibTeXing.load(inp)
     @test B isa BibTeX
